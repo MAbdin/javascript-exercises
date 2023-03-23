3b1606112e6fbb8c4b97763262d84447f108fffc
Zain Abdin <zabdin770@gmail.com>
2023-03-23 15:19:32 -0400

    Wrote a brute force solution to remove argument(s) from an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
 
+//Brute force solution to remove argument(s) from array.
+    let newArray =[];
+    for(let i = 0; i < myArray.length; i++){
+        let exists = false;
+        for(let j = 0; j < arguments.length; j++){
+            if (myArray[i] === arguments[j]){
+                exists = true;
+                break;
+            }
+        }
+        if(!exists){
+            newArray.push(myArray[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
