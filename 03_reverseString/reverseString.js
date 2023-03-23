b3a5d6cb35b64f7381e9ea62480e73b2dfe8aa11
Zain Abdin <zabdin770@gmail.com>
2023-03-23 14:33:35 -0400

    A progam to reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString ="";
+    for(let i = string.length; i >= 0; i--){
+        newString = newString + string.charAt(i);
+    }
+    return newString;
+
+    //return string.split("").reverse().join("");
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
